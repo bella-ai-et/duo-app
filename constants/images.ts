43c801081a766c88594d94d4e9141ba3c35b53a3
alex <jacobalex148@gmail.com>
2026-05-18 13:11:12 +0400

    implemenet the onboarding screen

---
+++ constants/images.ts
@@ -0,0 +1,17 @@
+import earth from "@/assets/images/earth.png";
+import mascotAuth from "@/assets/images/mascot-auth.png";
+import mascotLogo from "@/assets/images/mascot-logo.png";
+import mascotWelcome from "@/assets/images/mascot-welcome.png";
+import palace from "@/assets/images/palace.png";
+import streakFire from "@/assets/images/streak-fire.png";
+import treasure from "@/assets/images/treasure.png";
+
+export const images = {
+  mascotLogo,
+  mascotWelcome,
+  mascotAuth,
+  streakFire,
+  earth,
+  palace,
+  treasure,
+};
--- nativewind-env.d.ts
+++ nativewind-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="nativewind/types" />
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.svg";
+declare module "*.gif";
+declare module "*.webp";
